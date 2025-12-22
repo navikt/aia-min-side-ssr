4dc98395555503827ebe45846f1a95d08fbcdf98
Jostein Holje <jostein.holje@nav.no>
2025-12-22 14:41:53 +0100

    POST til oppslag-api v3

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,3 +1,4 @@
+import { parseIdportenToken } from '@navikt/oasis';
 import { logger } from './logger';
 
 export const fetchData = async (url: string, oboToken: string) => {
@@ -12,6 +13,31 @@ export const fetchData = async (url: string, oboToken: string) => {
 
   if (!response.ok) {
     logger.error(`Error fetching data from ${url} ${response.status} ${response.statusText}`);
+    throw new Error(`Http error with status: ${response.status}`);
+  }
+
+  return await response.json();
+};
+
+export const postTilOppslagV3API = async (url: string, oboToken: string) => {
+  logger.info(`Fetching (POST) data from ${url}`);
+  const parsed = parseIdportenToken(oboToken);
+  const identitetsnummer = parsed.ok ? parsed.pid : '';
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${oboToken}`,
+    },
+    body: JSON.stringify({
+      type: 'IDENIDENTITETSNUMMER',
+      identitetsnummer,
+    }),
+  });
+
+  if (!response.ok) {
+    logger.error(`Error fetching (POST) data from ${url} ${response.status} ${response.statusText}`);
     throw new Error(`Http error with status: ${response.status}`);
   }
 
