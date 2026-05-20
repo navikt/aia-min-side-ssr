e51f42545c45cad0c2c0cef694b23283ad7fa5b2
Jostein Holje <jostein.holje@nav.no>
2026-05-20 10:42:47 +0200

    @navikt/nav-dekoratoren-moduler@v4

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,11 +1,11 @@
 import { getAnalyticsInstance } from '@navikt/nav-dekoratoren-moduler';
 import { isLocal } from './environment.ts';
 
-export async function logUmamiEvent(eventName: string, data: unknown) {
+export async function logUmamiEvent(eventName: string, data: Record<string, unknown>) {
   try {
     const tracker = getAnalyticsInstance('aia-min-side-ssr');
     if (!isLocal) {
-      await tracker(eventName, data);
+      await tracker.custom(eventName, data);
     } else {
       console.log(`Logger til umami: ${eventName}`, data);
     }
